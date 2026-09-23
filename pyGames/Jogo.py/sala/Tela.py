2d8d1bef64f9e01524c153c6457660722be53875
Daniel-Francisc <francisco.santos@undf.edu.br>
2026-09-22 22:20:08 -0300

    a

--- pyGames/Jogo.py/sala/Tela.py
+++ pyGames/Jogo.py/sala/Tela.py
@@ -11,14 +11,14 @@ class Tela:
         self.debug = debug
         self.janela = tk.Tk()
         self.janela.title(titulo)
-        
+
         self.largura = largura
         self.altura = altura
         self.IsLargura = self.__Largura()
         self.IsAltura = self.__Altura()
-        
+
         self.janela.geometry(f"{self.IsLargura}x{self.IsAltura}")
-        
+
         self.canvas = tk.Canvas( 
             self.janela, 
             width=self.IsLargura,
@@ -27,7 +27,7 @@ class Tela:
             highlightthickness=0 
         )
         self.canvas.pack()
-        
+
     def __Largura(self):
         if self.largura == 0: 
             return self.janela.winfo_screenwidth()
@@ -36,13 +36,13 @@ class Tela:
         if self.altura == 0: 
             return self.janela.winfo_screenheight()
         else: return self.altura
-        
+
     def atualizar(self): pass
     def desenhar(self): self.canvas.delete("all")
-    
+
     def executar(self): 
         self.atualizar()
         self.desenhar()
-        
+
         self.janela.after(16, self.executar) 
         self.janela.mainloop()
---
+++ pyGames/Jogo.py/sala/gameplay.py
@@ -0,0 +1,6 @@
+import tkinter as tk
+import Tela
+
+class gameplay:
+    def __init__(self,altura,largura,titulo):
+        super.__init__(altura,largura,titulo)
